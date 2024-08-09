708d21a5759c2a95cea2af8ce1d17f8e093e57f5
bidianqing <bidianqing@qq.com>
2024-08-09 22:13:22 +0800

    *

--- AspNetCoreUseHuaweiCloudOBS/Controllers/HomeController.cs
+++ AspNetCoreUseHuaweiCloudOBS/Controllers/HomeController.cs
@@ -1,7 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using OBS;
 using OBS.Model;
-using System.Text;
 
 namespace AspNetCoreUseHuaweiCloudOBS.Controllers
 {
@@ -9,34 +8,29 @@ namespace AspNetCoreUseHuaweiCloudOBS.Controllers
     public class HomeController : ControllerBase
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ObsClient _obsClient;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ObsClient obsClient)
         {
             _logger = logger;
+            _obsClient = obsClient;
         }
 
         [HttpPost("upload")]
         public async Task<string> Upload([FromForm] IEnumerable<IFormFile> files)
         {
-            ObsConfig config = new ObsConfig();
-            config.Endpoint = "https://obs.cn-north-4.myhuaweicloud.com";
-
-            string accessKey = "";
-            string secretKey = "";
-            ObsClient client = new ObsClient(accessKey, secretKey, config);
-
             foreach (var file in files)
             {
-                using var stream = file.OpenReadStream();
-                
                 PutObjectRequest request = new PutObjectRequest
                 {
                     BucketName = "bidianqing",
                     ObjectKey = $"{DateTime.Now.Year}/{DateTime.Now.Month}/{Guid.NewGuid()}{Path.GetExtension(file.FileName)}",
-                    InputStream = stream
+                    ContentType = file.ContentType,
+                    ContentDisposition = file.ContentDisposition,
+                    InputStream = file.OpenReadStream()
                 };
-                var res = client.PutObject(request);
-                await stream.FlushAsync();
+                var res = _obsClient.PutObject(request);
+                await request.InputStream.FlushAsync();
             }
             
             return "OK";
--- AspNetCoreUseHuaweiCloudOBS/Program.cs
+++ AspNetCoreUseHuaweiCloudOBS/Program.cs
@@ -1,8 +1,18 @@
-var builder = WebApplication.CreateBuilder(args);
+using OBS;
 
+var builder = WebApplication.CreateBuilder(args);
+builder.WebHost.ConfigureKestrel((options) =>
+{
+    // Handle requests up to 47 MB
+    options.Limits.MaxRequestBodySize = 1024 * 1024 * 47;
+});
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddSingleton<ObsClient>(sp =>
+{
+    return new ObsClient("", "", new ObsConfig { Endpoint = "https://obs.cn-north-4.myhuaweicloud.com" });
+});
 
 var app = builder.Build();
 
